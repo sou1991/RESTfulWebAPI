52c90f46ee93c6d6c7c764a97adab768bc3c480e
sou_devlop <sou.business0109@gmail.com>
2020-10-18 09:12:56 +0900

    Add migration and Infrastructure layer

---
+++ RESTfulWebAPI/RESTfulWebAPI/Infrastructure/DataBaseService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RESTfulWebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulWebAPI.Infrastructure
+{
+    public class DataBaseService : DbContext,IDataBaseService
+    {
+        public DataBaseService(DbContextOptions<DataBaseService> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<MovieEntity> Movie { get; set; }
+        public void Save()
+        {
+            base.SaveChanges();
+        }
+    }
+}
---
+++ RESTfulWebAPI/RESTfulWebAPI/Infrastructure/IDataBaseService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RESTfulWebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulWebAPI.Infrastructure
+{
+    interface IDataBaseService
+    {
+        DbSet<MovieEntity> Movie { get; set; }
+        void Save();
+    }
+}
